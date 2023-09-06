6bf4849e2d53dec02e198a8dc4096f67f1c177e7
Lyst67 <p_listopad@ukr.net>
2023-09-06 11:59:47 +0300

    fix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,4 +1,4 @@
-import css from '../App.module.css'
+// import css from '../App.module.css'
 
 export const FeedbackOptions = ({options, onLeaveFeedback}) => {
     options = Object.entries(options)
